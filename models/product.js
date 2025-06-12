43a6e17a0a80a654f6805baf958e7c97f9c795d1
adamekholt <adam.ekholt@hotmail.com>
2025-06-12 21:55:02 +0200

    endringer i meny skjer med menu collection
    istedet for at nye produkter legger i en productscollection så legger de i menu

--- models/product.js
+++ models/product.js
@@ -31,6 +31,6 @@ const productSchema = new Schema({
     }
 });
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model('Product', productSchema, 'menu');
 
 export default Product;
